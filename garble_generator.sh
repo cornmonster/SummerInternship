328ae0e7ec6da6ea6e1c0f696f00f7bc0381ffa3
Jon Wang <zhongwang.dt@gmail.com>
2017-05-28 15:18:11 -0400

    updated garble_generator.sh

--- garble_generator.sh
+++ garble_generator.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 num_file=0
+num_dir=0
 
-cd "$2"
-
-while [ "$num_file" -lt "$1" ]
+while [ "$num_dir" -lt "$1" ]
 do
-	pwgen 10000 1 > "garble_file_$num_file"
-	num_file=$((num_file+1))
+	mkdir "subdir$num_dir"
+	cd "subdir$num_dir"
+
+	while [ "$num_file" -lt "$2" ]
+	do
+		pwgen 10000 1 > "garble_file_$num_file"
+		num_file=$((num_file+1))
+	done
+
+	num_dir=$((num_dir+1))
+	cd ..
 done
 
-cd ..
 echo "+OK Garble files created."
