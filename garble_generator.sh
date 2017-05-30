00a1287cc8d39bc01c32c75ca8296772c74b5413
Jon Wang <zhongwang.dt@gmail.com>
2017-05-30 17:45:15 -0400

    updated garble generator

--- garble_generator.sh
+++ garble_generator.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 num_file=0
 num_dir=0
+file_prefix="$3"
 
 echo "Start creating subdir and files..."
 
 while [ "$num_file" -lt "$2" ]
 do
-	pwgen 10000 1 > "garble_file_$num_file"
+	pwgen 10000 1 > "garble_file_$file_prefix$num_file"
 	num_file=$((num_file+1))
 done
 
 while [ "$num_dir" -lt "$1" ]
 do
-	mkdir "subdir$num_dir"
-	cd "subdir$num_dir"
-	num_dir=0
+	mkdir "subdir_$file_prefix$num_dir"
+	echo "---subdir_$file_prefix$num_dir created"
+	cd "subdir_$file_prefix$num_dir"
+	num_file=0
 	while [ "$num_file" -lt "$2" ]
 	do
-		pwgen 10000 1 > "garble_file_$num_file"
+		pwgen 10000 1 > "garble_file_$file_prefix$num_file"
 		num_file=$((num_file+1))
 	done
 
-	echo "---subdir$num_dir created"
+
 	num_dir=$((num_dir+1))
 	cd ..
 done
